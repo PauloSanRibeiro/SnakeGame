20415a5727743fd0ee3bbacf8ba6cd1acb59c9a6
Paulin_ho <paulo_pr11@hotmail.com>
2021-01-02 17:33:36 -0300

    Criando drawfood

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake [0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -18,6 +22,11 @@ function criarCobrinha(){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
+}
+
+function drawfood(){
+    context.fillstyle = "black";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 
@@ -38,6 +47,7 @@ function iniciarjogo(){
     
     criarBG();
     criarCobrinha();
+    drawfood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
